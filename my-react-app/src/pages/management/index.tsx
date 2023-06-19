be10eb9d60cd28c7425426b027c726bcc8ba0002
thiago <thiagodesouza.dev@gmail.com>
2023-06-19 00:38:14 -0300

    feat: page admin

--- my-react-app/src/pages/management/index.tsx
+++ my-react-app/src/pages/management/index.tsx
@@ -1,11 +1,126 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/header";
+import EmployeeServiceModal from "./modal/EmployeeServiceModal";
+import MedicalEmployeeModal from "./modal/MedicalEmployeeModal";
+import { Button } from "react-bootstrap";
+import ConfirmationModal from "./modal/ConfirmationModal";
 
+type CreateEmployeeService = {
+  cpf: string;
+  rg: string;
+  email: string;
+};
+
+type Employee = {
+  id: number;
+  nome: string;
+  cargo: string;
+};
+
+type CreateEmployeeMedic = {
+  nome: string;
+  cpf: string;
+  rg: string;
+  cargo: string;
+  especialidade: string;
+  docTipo: string;
+  docNumero: string;
+  cns: string;
+  mat: string;
+  email: string;
+};
 function Management() {
+  const [showModalService, setShowModalService] = useState(false);
+  const [showModalDoctor, setShowModalDoctor] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  const handleSaveEmployeeService = (employee: CreateEmployeeService) => {
+    // Lógica para salvar o funcionário
+    console.log(employee);
+  };
+  const handleSaveEmployeeDoctor = (employee: CreateEmployeeMedic) => {
+    // Lógica para salvar o funcionário
+    console.log(employee);
+  };
+
+  const handleConfirmDeleteEmployee = (id: number) => {
+    console.log(id);
+
+    // Lógica para excluir o funcionário com o ID fornecido
+
+    setShowDeleteModal(false);
+  };
+  const employees: Employee[] = [
+    { id: 1, nome: "João", cargo: "Atendente" },
+    { id: 2, nome: "Maria", cargo: "Médica" },
+    { id: 3, nome: "Pedro", cargo: "Enfermeiro" },
+  ];
+
+  const keys = Object.keys(employees[0]) as (keyof Employee)[];
+
   return (
     <div>
       <Header />
-      Management
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          marginTop: "20px",
+          marginBottom: "20px",
+        }}
+      >
+        <Button onClick={() => setShowModalService(true)} variant="primary">
+          Registrar Funcionário de atendimento
+        </Button>
+        <EmployeeServiceModal
+          show={showModalService}
+          onClose={() => setShowModalService(false)}
+          onSave={handleSaveEmployeeService}
+        />
+        <Button onClick={() => setShowModalDoctor(true)} variant="primary">
+          Registrar Funcionário de Medicina
+        </Button>
+        <MedicalEmployeeModal
+          show={showModalDoctor}
+          onClose={() => setShowModalDoctor(false)}
+          onSave={handleSaveEmployeeDoctor}
+        />
+      </div>
+
+      <table className="table table-bordered table-striped">
+        <thead className="table-dark">
+          <tr>
+            {keys.map((key) => (
+              <th key={key}>{key}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((employee) => (
+            <tr key={employee.id}>
+              {keys.map((key) => (
+                <td key={key}>{employee[key]}</td>
+              ))}
+              <td>
+                <Button variant="primary" style={{ marginRight: "10px" }}>
+                  Editar
+                </Button>
+                <Button
+                  onClick={() => setShowDeleteModal(true)}
+                  variant="danger"
+                >
+                  Deletar
+                </Button>
+                <ConfirmationModal
+                  show={showDeleteModal}
+                  onCancel={() => setShowDeleteModal(false)}
+                  onConfirm={handleConfirmDeleteEmployee}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ my-react-app/src/pages/management/modal/MedicalEmployeeModal.tsx
@@ -0,0 +1,174 @@
+import React, { useState } from "react";
+import { Button, Col, Form, Modal, Row } from "react-bootstrap";
+
+type MedicalEmployee = {
+  nome: string;
+  cpf: string;
+  rg: string;
+  cargo: string;
+  especialidade: string;
+  docTipo: string;
+  docNumero: string;
+  cns: string;
+  mat: string;
+  email: string;
+};
+
+type Props = {
+  show: boolean;
+  onClose: () => void;
+  onSave: (employee: MedicalEmployee) => void;
+};
+
+const MedicalEmployeeModal: React.FC<Props> = ({ show, onClose, onSave }) => {
+  const [nome, setNome] = useState("");
+  const [cpf, setCPF] = useState("");
+  const [rg, setRG] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [especialidade, setEspecialidade] = useState("");
+  const [docTipo, setDocTipo] = useState("");
+  const [docNumero, setDocNumero] = useState("");
+  const [cns, setCNS] = useState("");
+  const [mat, setMat] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const employee: MedicalEmployee = {
+      nome,
+      cpf,
+      rg,
+      cargo,
+      especialidade,
+      docTipo,
+      docNumero,
+      cns,
+      mat,
+      email,
+    };
+
+    onSave(employee);
+    onClose();
+  };
+
+  return (
+    <Modal show={show} onHide={onClose}>
+      <Modal.Dialog className="modal-lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Registrar Funcionário Médico</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group as={Col} controlId="nome">
+              <Form.Label>Nome</Form.Label>
+              <Form.Control
+                type="text"
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+                style={{ width: "100%" }}
+              />
+            </Form.Group>
+            <Row>
+              <Form.Group as={Col} controlId="cpf">
+                <Form.Label>CPF</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={cpf}
+                  onChange={(e) => setCPF(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group as={Col} controlId="rg">
+                <Form.Label>RG</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={rg}
+                  onChange={(e) => setRG(e.target.value)}
+                />
+              </Form.Group>
+            </Row>
+            <Row>
+              <Form.Group as={Col} controlId="cargo">
+                <Form.Label>Cargo</Form.Label>
+                <Form.Control
+                  as="select"
+                  value={cargo}
+                  onChange={(e) => setCargo(e.target.value)}
+                >
+                  <option value="">Selecione o cargo</option>
+                  <option value="Médico">Médico</option>
+                  <option value="Enfermeiro">Enfermeiro</option>
+                  <option value="Técnico em Enfermagem">
+                    Técnico em Enfermagem
+                  </option>
+                </Form.Control>
+              </Form.Group>
+              <Form.Group as={Col} controlId="especialidade">
+                <Form.Label>Especialidade</Form.Label>
+                <Form.Control
+                  as="select"
+                  value={especialidade}
+                  onChange={(e) => setEspecialidade(e.target.value)}
+                >
+                  <option value="">Selecione a especialidade</option>
+                  <option value="Cardiologia">Cardiologia</option>
+                  <option value="Dermatologia">Dermatologia</option>
+                  <option value="Pediatria">Pediatria</option>
+                </Form.Control>
+              </Form.Group>
+            </Row>
+            <Row>
+              <Form.Group as={Col} controlId="docTipo">
+                <Form.Label>Doc. MED/Tipo</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={docTipo}
+                  onChange={(e) => setDocTipo(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group as={Col} controlId="docNumero">
+                <Form.Label>Doc. N°</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={docNumero}
+                  onChange={(e) => setDocNumero(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group as={Col} controlId="cns">
+                <Form.Label>CNS</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={cns}
+                  onChange={(e) => setCNS(e.target.value)}
+                />
+              </Form.Group>
+            </Row>
+
+            <Form.Group as={Col} controlId="mat">
+              <Form.Label>Mat</Form.Label>
+              <Form.Control
+                type="text"
+                value={mat}
+                onChange={(e) => setMat(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group as={Col} controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Salvar
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal.Dialog>
+    </Modal>
+  );
+};
+
+export default MedicalEmployeeModal;
