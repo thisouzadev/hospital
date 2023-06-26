5d165294960a33fb3bfe6fca6460bc02c8420cda
kortkamp <marcelusmedius@gmail.com>
2023-06-26 08:30:25 -0300

    refactor: use api

--- my-react-app/src/service/doctors.service.ts
+++ my-react-app/src/service/doctors.service.ts
@@ -1,18 +1,13 @@
-import axios from 'axios';
+import api from './api';
 
 const doctorsService = {
 
   async getAll() {
-    try {
-      const response = await axios({
-        method: 'get',
-        url: 'http://localhost:3000/doctors',
-      });
-      return response.data;
-    } catch (error: any) {
-      return error.response?.data;
-    }
+    const response = await api.get('/doctors');
+
+    return response.data;
   },
+
 };
 
 export default doctorsService;
