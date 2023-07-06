6de31b4a50b15c5c17c8bd775836d74f4333d2f4
kortkamp <marcelusmedius@gmail.com>
2023-07-05 21:56:39 -0300

    chore: remove not used code

--- backend/src/modules/tratamento/repositories/tratamento.repository.ts
+++
@@ -1,7 +0,0 @@
-import { Repository } from 'typeorm';
-import { EntityRepository } from 'typeorm';
-
-import { Tratamento } from '../entities/tratamento.entity';
-
-@EntityRepository(Tratamento)
-export class TratamentoRepository extends Repository<Tratamento> {}
